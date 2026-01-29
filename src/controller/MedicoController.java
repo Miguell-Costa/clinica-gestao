8d3fb647090986f2e39fe4cce56e20af9b26c68d
Miguell-Costa <jmmc436@gmail.com>
2026-01-29 20:20:31 +0000

    add MedicoController.java

---
+++ src/controller/MedicoController.java
@@ -0,0 +1,68 @@
+package controller;
+
+import dao.MedicoDAO;
+import model.Medico;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MedicoController {
+    private MedicoDAO medicoDAO;
+
+    public MedicoController(MedicoDAO medicoDAO) {
+        this.medicoDAO = medicoDAO;
+    }
+
+    // metodo para criar medico
+    public void inserirMedico(String nome, String especialidade, String email){
+        if(!validarEmail(email)){
+            System.out.println("Email Inválido");
+            return;
+        }
+
+        Medico medico = new Medico(nome, especialidade, email);
+        medicoDAO.create(medico);
+    }
+
+    // metodo para eliminar medico
+    public void eliminarMedico(int id){
+        if (!medicoDAO.existeMedico(id)){
+            System.out.println("Não existe nenhum paciente com esse id!");
+            return;
+        }
+
+        medicoDAO.delete(id);
+    }
+
+    // metodo para atualizar medico
+    public void atualizarMedico(int id, String nome, String especialidade, String email){
+        if (!medicoDAO.existeMedico(id)){
+            System.out.println("Não existe nenhum paciente com esse id!");
+            return;
+        }
+        if(!validarEmail(email)){
+            System.out.println("Email Inválido");
+            return;
+        }
+
+        Medico medico = new Medico(nome, especialidade, email);
+        medicoDAO.update(id, medico);
+    }
+
+    // metodo para listar medicos
+    public void listarMedicos(){
+        List<Medico> medicos = new ArrayList<>();
+        medicos = medicoDAO.read();
+
+        for (Medico m: medicos){
+            System.out.println(m);
+        }
+    }
+
+    // metodo para verificar um email
+    public boolean validarEmail(String email){
+        String regex = "^[\\w.-]+@([a-zA-Z-]+\\.)+[a-zA-Z]{2,}$";
+        return Pattern.matches(regex, email);
+    }
+}
